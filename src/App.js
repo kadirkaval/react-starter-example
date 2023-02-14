20f6aa970e92950e7232c06d9052615ea225e269
kadir kaval <60962814+kadirkaval@users.noreply.github.com>
2023-02-15 02:18:51 +0300

    tailwind css add project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-lime-300 text-yellow-500 font-semibold mx-auto w-full">
      <p>Test</p>
      <p>
      {
         process.env.REACT_APP_API_URL
       }
      </p>
+    <button className='w-[200px] rounded py-2 px-7 bg-blue-600 text-white hover:bg-blue-800 transition duration-150 ease-in-out text-xl'>Gönder</button>
     </div>
   );
 }
