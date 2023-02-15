ecb4e7f6b059fde6d16e16b205a8536b6f837e57
kadir kaval <60962814+kadirkaval@users.noreply.github.com>
2023-02-15 21:21:56 +0300

    useEffect and useState example

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { useState } from 'react';
 import './App.css';
-import Tab from './components/Tab';
+import Test from './components/Test';
 import "./index.css";
 
 
 function App() {
-  const [activeTab, setActiveTab] = useState(0);
+  // const [activeTab, setActiveTab] = useState(0);
+  const [show, setShow]=useState(false);
+  
   return (
     <div>
-      <button onClick={()=>setActiveTab((Math.random()*2).toFixed(0))}>Aktif tabı değiştir</button>
+      {/* <button onClick={()=>setActiveTab((Math.random()*2).toFixed(0))}>Aktif tabı değiştir</button>
      <Tab activeTab={activeTab} setActiveTab={setActiveTab}>
         <Tab.Panel title="Profile">1.Tab</Tab.Panel>
         <Tab.Panel title="Hakkında">2.Tab</Tab.Panel>
@@ -18,7 +20,12 @@ function App() {
       activeTab === 2 && (
         <div>Burasıda ektra bir alan</div>
       )
-     }
+     } */}
+     <button onClick={()=>setShow(!show)}>{show?"Gizle":"Göster"}</button>
+    {
+      show && ( <Test/>)
+    }
+   
     </div>
   );
 }
